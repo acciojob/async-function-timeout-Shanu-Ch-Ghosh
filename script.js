63eb9b22399070300a641dc583d616a4d4a57a8b
Shanu Chandra Ghosh <166943936+Shanu-Ch-Ghosh@users.noreply.github.com>
2024-08-03 10:52:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+let text = document.getElementById("text");
+let delay =  document.getElementById("delay");
+let btn =  document.getElementById("btn");
+let output = document.getElementById("output");
+
+btn.addEventListener("click", ()=>{
+	async function asyncCall(){
+		let res = await getPromise();
+		console.log(res)
+	}
+
+	asyncCall();
+})
+
+function getPromise() {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(output.innerText= text.value);
+		},delay.value)
+	})
+}
